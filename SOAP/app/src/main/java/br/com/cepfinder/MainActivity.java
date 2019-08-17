f4e6457c3e621d16390554319bdbbb7961d371bf
André Victor Stephanes Valvassori <an.victor@hotmail.com>
2019-08-17 14:53:54 -0300

    Soap folder change

--- SOAP/app/src/main/java/br/com/cepfinder/MainActivity.java
+++ SOAP/app/src/main/java/br/com/cepfinder/MainActivity.java
@@ -15,20 +15,8 @@ import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
-import br.com.cepfinder.Model.CEP;
-import br.com.cepfinder.Retrofit.RetrofitInitialization;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
-import static org.ksoap2.serialization.MarshalHashtable.NAMESPACE;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editText;
-    private Button button;
-    private TextView textView;
 
     private EditText editSoap;
     private Button buttonSoap;
@@ -45,43 +33,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editText);
-        button = findViewById(R.id.button2);
-        textView = findViewById(R.id.textView);
 
         buttonSoap = findViewById(R.id.buttonSoap);
         resultadoSoap = findViewById(R.id.resultSoap);
         editSoap = findViewById(R.id.editSoap);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadCep(editText.getText().toString());
-
-            }
-        });
-
         buttonSoap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new CallWebService().execute(editSoap.getText().toString());
-            }
-        });
-    }
-
-    private void loadCep(String cep) {
-        Call<CEP> call = new RetrofitInitialization().getCepJsonService().getCepJson(cep);
-        call.enqueue(new Callback<CEP>() {
-            @Override
-            public void onResponse(Call<CEP> call, Response<CEP> response) {
-                CEP endereco = response.body();
-                textView.setText(endereco.getLogradouro() + ", " + endereco.getBairro() + " - " + endereco.getLocalidade() + "/" + endereco.getUf());
-            }
-
-            @Override
-            public void onFailure(Call<CEP> call, Throwable t) {
-                textView.setText("CEP não encontrado!");
             }
         });
     }
